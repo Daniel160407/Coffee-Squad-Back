1c39ad6832ee5efdb177116e9a660df88ee6195f
GiorgiBazerashvili <giorgibazerashvili5791@gmail.com>
2025-10-19 01:31:55 +0400

    workout cards creator added

--- src/controllers/workout.controller.js
+++ src/controllers/workout.controller.js
@@ -1,6 +1,53 @@
-// import mongoose from "mongoose";
-// import Workout from "../models/workout.model";
+import mongoose from "mongoose";
+import Workout from "../models/workout.model.js";
 
-// export async function createWorkoutCard(req, res) {
-//   const {}
-// }
+export async function createWorkoutCard(req, res) {
+  try {
+    const {
+      type,
+      distance,
+      pace,
+      notes,
+      title,
+      totalDuration,
+      caloriesBurned,
+    } = req.body;
+
+    const userID = req.user._id;
+
+    const card = await Workout.create({
+      type,
+      distance,
+      pace,
+      notes,
+      title,
+      totalDuration,
+      caloriesBurned,
+      userId: userID,
+    });
+
+    res.staus(201).json({
+      success: true,
+      message: "workout card created successfully",
+      data: card,
+    });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ success: false, message: error.message, data: error });
+  }
+}
+
+export async function getWorkoutCards(req, res) {
+  try {
+    const cards = await Workout.find({});
+
+    res
+      .status(200)
+      .json({ success: true, message: "found some cards", data: cards });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ success: false, message: error.message, data: error });
+  }
+}
--- src/routes/workout.routes.js
+++ src/routes/workout.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
-// import { createWorkoutCard } from "../controllers/workout.controller.js";
+import {
+  createWorkoutCard,
+  getWorkoutCards,
+} from "../controllers/workout.controller.js";
 
 const router = Router();
 
 router.use(protectRoute);
 
-// router.post("/createcard", createWorkoutCard);
+router.get("/getcards", getWorkoutCards);
+router.post("/createcard", createWorkoutCard);
 
 export default router;
