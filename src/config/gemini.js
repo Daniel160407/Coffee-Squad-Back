f4822de77c174860f281b54f561335825c1d18b3
GeorgeShani <shanidze.geo2004@gmail.com>
2025-10-18 16:49:56 +0400

    fix gemini

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -3,9 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const genAI = new GoogleGenerativeAI({
-  apiKey: process.env.GEMINI_API_KEY,
-});
-
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 export default model;
